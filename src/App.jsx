b7d6538c9945fb5c8df1f928fabbbd83b125b999
mshalom27 <menshalom07@gmail.com>
2025-11-04 03:17:30 +0530

    hints, correct, wrong, finish pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Button } from "./components/ui/button";
 import { ThemeProvider } from "@/components/theme-provider";
+import HintBox from "./components/hints";
+import FinalAnswerPopup from "./components/finish";
+import CorrectAnswerPopup from "./components/correct";
+import WrongAnswerPopup from "./components/wrong";
 
 function App() {
     return (
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
             <main className="flex min-h-screen flex-col items-center justify-center p-4">
                 Flag in the Blue
-                <Button>DSCxOC</Button>
+                 <Button>DSCxOC</Button>
+                <HintBox /> 
+                {/* <CorrectAnswerPopup/>
+                <FinalAnswerPopup/>
+                <WrongAnswerPopup/>  */}
+
+
             </main>
         </ThemeProvider>
     );
